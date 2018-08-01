4ea9e69187d783354b1ec8f25ae2716fa74f2998
Robiel <robielasgedom@gmail.com>
2018-07-31 23:10:42 -0400

    spotify

--- liri.js
+++ liri.js
@@ -29,3 +29,23 @@ var getMeSpotify = function (songName) {
         }
     });
 }
+
+var getMeSpotify = function (songName) {
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            console.log("Error " + err);
+            return;
+        }
+
+        var songs = data.track.items;
+        for(var i = 0; i < songs.length; i++) {
+            console.log(i);
+            console.log('artist(s): ' + songs[i].artists.map(getArtistsName));
+            console.log('song name: ' + songs[i].name);
+            console.log('preview song' + songs[i].preview_url);
+            console.log('album: ' + songs[i].album.name);
+            console.log('*******************************************************');
+        }
+    });
+}
