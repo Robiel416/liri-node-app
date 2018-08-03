6e89e8d7863bae0c1c80951fdb4e17e2bc23f476
Robiel <robielasgedom@gmail.com>
2018-08-02 23:28:11 -0400

    fix

--- keys.js
+++ keys.js
@@ -1,7 +1,12 @@
 console.log('page loaded');
 
 exports.spotify = {
-    id: process.env.042940c01d3c4e5faf5bcae06c51acb5,
-    secret: process.env.8f69aa11472644eda84072375ad3aef1
+    id: '042940c01d3c4e5faf5bcae06c51acb5',
+    secret: '8f69aa11472644eda84072375ad3aef1'
   };
-  
+ 
+
+  //OMDB Key
+exports.omdbKey = {
+	key: 'fd53cb58'
+};
--- liri.js
+++ liri.js
@@ -1,51 +1,101 @@
-require("dotenv").config();
-
+var fs = require("fs");
 var keys = require("./keys.js");
-
-var spotify = new Spotify(keys.spotify);
-
+var Spotify = require("node-spotify-api");
 var request = require("request");
+var command = process.argv[2];
+var titles = process.argv[3];
 
-var getArtistsName = function (artist) {
-    return artist.name;
+
+switch (command) {
+    case "spotify-this-song":
+        getSpotify();
+        break;
+    case "movie-this":
+        getMovie();
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+    default: console.log("That is not a valid command");
 }
 
-var getMeSpotify = function (songName) {
+function getSpotify() {
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+    var songTitle = titles;
 
-    spotify.search({ type: 'track', query: songName }, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
-            return;
-        }
+    if (!songTitle) {
+        songTitle = "The Sign";
+    }
 
-        var songs = data.track.items;
-        for(var i = 0; i < songs.length; i++) {
-            console.log(i);
-            console.log('artist(s): ' + songs[i].artists.map(getArtistsName));
-            console.log('song name: ' + songs[i].name);
-            console.log('preview song' + songs[i].preview_url);
-            console.log('album: ' + songs[i].album.name);
-            console.log('*******************************************************');
+    var songName = songTitle;
+    spotify.search({ type: 'track', query: songName }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + err);
+        } else {
+            console.log("Searched: " + songName.toUpperCase());
+            console.log("\n------------Song Description-----------\n");
+            console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+            console.log("Song Name: " + songName);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Spotify preview link: " + data.tracks.items[0].album.external_urls.spotify);
+
         }
     });
 }
 
-var getMeSpotify = function (songName) {
 
-    spotify.search({ type: 'track', query: songName }, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
+function getMovie() {    
+    var omdb = keys.omdbKey.key;
+    var movieTitle = titles;
+
+    if (!movieTitle) {
+        movieTitle = "mr nobody";
+    }
+
+    var name = movieTitle;
+    request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=" + omdb, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            console.log("\n ====  YOUR MOVIE ====\n");
+            console.log("Movie Title: " + data.Title);
+            console.log("Release Year: " + data.Year);
+            console.log("IMDB Rating: " + data.Ratings[0].Value);
+            console.log("Rotten Tomato Rating: " + data.Ratings[1].Value);
+            console.log("Country: " + data.Country);
+            console.log("Language: " + data.Language);
+            console.log("Plot: " + data.Plot);
+            console.log("Actors: " + data.Actors);
+        } else {
+            console.log('error:', error);
             return;
-        }
-
-        var songs = data.track.items;
-        for(var i = 0; i < songs.length; i++) {
-            console.log(i);
-            console.log('artist(s): ' + songs[i].artists.map(getArtistsName));
-            console.log('song name: ' + songs[i].name);
-            console.log('preview song' + songs[i].preview_url);
-            console.log('album: ' + songs[i].album.name);
-            console.log('*******************************************************');
         }
     });
 }
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (!error) {
+            var dataArr = data.split(",");
+            var randomCommand = dataArr[0];
+            var randomTitle = dataArr[1];
+
+            switch (randomCommand) {
+                case "spotify-this-song":
+                    titles = randomTitle;
+                    getSpotify();
+                    break;
+                case "movie-this":
+                    titles = randomTitle;
+                    getMovie();
+                    break;
+                default: console.log("That is not a valid command.");
+            }
+        } else {
+            throw error;
+        }
+    });
+}
+
